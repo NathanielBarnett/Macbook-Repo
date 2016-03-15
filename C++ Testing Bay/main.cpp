f381d63213f1a2ed27aad7a9264d154bb3cc5fa8
Nathan <Nathan@mobile-squire.kc.umkc.edu>
2016-03-15 12:23:07 -0500

    Initial Commit

---
+++ C++ Testing Bay/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ Testing Bay
+//
+//  Created by Nathan on 3/15/16.
+//  Copyright © 2016 Nathan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
